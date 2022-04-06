b19e2121b9eee0ab4a7b7a3705508388767645c0
Marcus Castilho <rootmarcus001@gmail.com>
2022-04-06 16:58:33 -0300

    Tela inicial 5.0 criada

--- js/functions.js
+++
@@ -1,6 +0,0 @@
-function download() {
-    var link = document.createElement("a");
-    link.download = "Auto-Clone";
-    link.href = "https://github.com/c4st1lh0/Auto-Clone/archive/refs/heads/main.zip";
-    link.click();
-  }
